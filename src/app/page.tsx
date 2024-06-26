7827bce534508d8f01e41b91e2769cf3ca4fb8ae
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-06-26 10:06:25 +0530

    Adding Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,16 @@ import Hero from "./components/Hero";
 import Jobs from "./components/Jobs";
 
 
-export default function Home() {
+export default async function Home() {
+  // Retrieves the user from the session or returns `null` if no user is signed in
+  const { user } = await getUser();
+
+  // Get the URL to redirect the user to AuthKit to sign in
+  const signInUrl = await getSignInUrl();
+
+  // Get the URL to redirect the user to AuthKit to sign up
+  const signUpUrl = await getSignUpUrl();
+  
   return (
     <>
     <Hero/>
