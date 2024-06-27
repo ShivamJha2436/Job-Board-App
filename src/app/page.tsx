4addda1f224e4f4d1ca37b7f3faa190d729cf670
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-06-27 08:38:49 +0530

    Addig authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Hero from "./components/Hero";
 import Jobs from "./components/Jobs";
-
+import {
+  getSignInUrl,
+  getSignUpUrl,
+  getUser,
+  signOut,
+} from '@workos-inc/authkit-nextjs';
 
 export default async function Home() {
   // Retrieves the user from the session or returns `null` if no user is signed in
@@ -11,6 +16,8 @@ export default async function Home() {
 
   // Get the URL to redirect the user to AuthKit to sign up
   const signUpUrl = await getSignUpUrl();
+
+  
   
   return (
     <>
