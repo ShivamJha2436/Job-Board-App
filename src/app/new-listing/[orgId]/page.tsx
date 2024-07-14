539a9f6c37ee29a3fce3118969e7d6314e46caed
ShivamJha2436 <shivamkumar87148@gmail.com>
2024-07-14 21:26:54 +0530

    Fixing some bugs

--- src/app/new-listing/[orgId]/page.tsx
+++ src/app/new-listing/[orgId]/page.tsx
@@ -1,8 +1,14 @@
+import { RadioGroup, TextArea, TextField, Theme } from '@radix-ui/themes';
 import { getUser } from "@workos-inc/authkit-nextjs";
 import { WorkOS } from "@workos-inc/node";
 import '@radix-ui/themes/styles.css';
-import { TextField, Theme } from '@radix-ui/themes';
-
+import {
+  CitySelect,
+  CountrySelect,
+  StateSelect,
+  LanguageSelect,
+} from 'react-country-state-city';
+import "react-country-state-city/dist/react-country-state-city.css";
 
 type PageProps = {
   params: {
@@ -29,9 +35,31 @@ export default async function NewListingForOrgPage(props: PageProps) {
     <Theme>
       <form
         action=""
-        className="container mt-6"
+        className="container mt-6 flex flex-col gap-4"
       >
         <TextField.Root placeholder="Job Title"/>
+        <div className="flex gap-4 ">
+          <div className="font-medium">
+            Remote?
+            <RadioGroup.Root defaultValue="hybrid" name="example">
+                <RadioGroup.Item value="onsite">On-site</RadioGroup.Item>
+                <RadioGroup.Item value="hybrid">Hybrid-Remote</RadioGroup.Item>
+                <RadioGroup.Item value="remote">Fully Remote</RadioGroup.Item>
+            </RadioGroup.Root>
+          </div>
+          <div className="font-medium">
+            Full-Time?
+            <RadioGroup.Root defaultValue="full" name="example">
+                <RadioGroup.Item value="project">Project</RadioGroup.Item>
+                <RadioGroup.Item value="part">Part-time</RadioGroup.Item>
+                <RadioGroup.Item value="full">Full-time</RadioGroup.Item>
+            </RadioGroup.Root>
+          </div>
+          <div>
+            Location
+          </div>
+        </div>
+        <TextArea placeholder="Job Description" resize="vertical"/>
       </form>
     </Theme>
   );
